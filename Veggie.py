6261fca1da1bdcb28b91a5da1d445ce58a9035d9
Shachee S B <47169110+shacheesb@users.noreply.github.com>
2023-12-03 23:01:51 +0530

    Add files via upload

---
+++ Veggie.py
@@ -0,0 +1,22 @@
+from FieldInhabitant import FieldInhabitant
+
+class Veggie(FieldInhabitant):
+    def __init__(self, name, symbol, points):
+        super().__init__(symbol)
+        self._name = name
+        self._points = points
+
+    def __str__(self):
+        return f"Symbol: {self.get_symbol()}, Name: {self._name}, Points: {self._points}"
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_points(self):
+        return self._points
+
+    def set_points(self, points):
+        self._points = points
