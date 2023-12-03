c1a21b71f17606b63740b0e8c183925db44f3ee2
Shachee S B <47169110+shacheesb@users.noreply.github.com>
2023-12-03 22:42:23 +0530

    Add files via upload

---
+++ FieldInhabitant.py
@@ -0,0 +1,11 @@
+class FieldInhabitant:
+    def __init__(self, symbol):
+        self._symbol = symbol
+
+    def get_symbol(self):
+        return self._symbol
+
+    def set_symbol(self, symbol):
+        self._symbol = symbol
+
+
