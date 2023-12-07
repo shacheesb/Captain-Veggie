28f607a39338a1b499db654da625b88bb23ccf75
Shachee S B <47169110+shacheesb@users.noreply.github.com>
2023-12-08 02:32:35 +0530

    Update FieldInhabitant.py

--- FieldInhabitant.py
+++ FieldInhabitant.py
@@ -1,6 +1,8 @@
 # Author: Shachee SB, Pranav Parekh
 # Date: 5th December 2023
 # Description: Defines the field inhabitant i.e rabbit, veggie, captain
+
+#A constructor that takes in a parameter representing a text symbol for the field inhabitant (vegetable, rabbit, captain, etc) and which assigns that value to a new member variable 
 class FieldInhabitant:
     def __init__(self, symbol):
         self._symbol = symbol
