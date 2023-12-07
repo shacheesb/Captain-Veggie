04e856b15df826a9825533a8111dbac5ddc0f837
Shachee S B <47169110+shacheesb@users.noreply.github.com>
2023-12-08 01:57:39 +0530

    Update Veggie.py

--- Veggie.py
+++ Veggie.py
@@ -6,15 +6,16 @@ game.
 from FieldInhabitant import FieldInhabitant
 
 class Veggie(FieldInhabitant):
+    #private variables
     def __init__(self, name, symbol, points):
         super().__init__(symbol)
         self._name = name
         self._points = points
-
-    def __str__(self):
+        
+    def __str__(self): 
         return f"Symbol: {self.get_symbol()}, Name: {self._name}, Points: {self._points}"
 
-    def get_name(self):
+    def get_name(self): #Appropriate getter setter functions
         return self._name
 
     def set_name(self, name):
