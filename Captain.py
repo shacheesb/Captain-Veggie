12c61c61052a82aa04c026da5c93916f2cdeabf9
Shachee S B <47169110+shacheesb@users.noreply.github.com>
2023-12-08 02:35:03 +0530

    Update Captain.py

--- Captain.py
+++ Captain.py
@@ -3,12 +3,12 @@
 # Description: Sub class of Creature.
 
 from Creature import Creature
-
+# A constructor that takes in two integer values representing x and y coordinates 
 class Captain(Creature):
     def __init__(self, x, y):
         super().__init__(x, y, "V")
         self._collected_veggies = []
-
+# A function that takes in a Veggie object as a parameter, returns nothing, and adds the object to the List of Veggie objects 
     def add_veggie(self, veggie):
         self._collected_veggies.append(veggie)
 
